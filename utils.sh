b89dfce99e598044776a84903eeb45d50b47fbe5
Z User <z@container>
2026-02-17 15:58:10 +0000

    feat: Consolidar para branch main única
    - Atualizados workflows para usar branch main
    - Removidas referências à branch update
    - build.md agora commita na main
    - Magisk update json aponta para main

--- utils.sh
+++ utils.sh
@@ -645,7 +645,7 @@ build_rv() {
                         "${app_name} ${args[rv_brand]}" \
                         "${version} (patches ${patches_ver%%.$PATCH_EXT})" \
                         "${app_name} ${args[rv_brand]} Magisk module" \
-                        "https://raw.githubusercontent.com/${GITHUB_REPOSITORY-}/update/${upj}" \
+                        "https://raw.githubusercontent.com/${GITHUB_REPOSITORY-}/main/${upj}" \
                         "$base_template"
 
                 local module_output="${app_name_l}-${rv_brand_f}-magisk-v${version_f}-${arch_f}.zip"
